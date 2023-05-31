fd285b1c52694245bd614b53cf01ce09c24771e3
Marko Maksimovic 1419S <mmaksimovic1419s@raf.rs>
2023-05-31 02:00:41 +0200

    Initial setup

---
+++ rvas_projekat_solution/rvas_projekat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace rvas_projekat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
